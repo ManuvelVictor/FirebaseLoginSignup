7e3ba43e6912149ba9b005a02d282d199cb5a1da
Manuvel Victor <81816979+ManuvelVictor@users.noreply.github.com>
2023-04-10 01:30:29 +0530

    Implemented the login and sign up with firebase authentication created new icon for the app

--- app/src/main/java/com/victor/firebaseloginsignup/LoginActivity.java
+++ app/src/main/java/com/victor/firebaseloginsignup/LoginActivity.java
@@ -1,14 +1,64 @@
 package com.victor.firebaseloginsignup;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.victor.firebaseloginsignup.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        // Initialize view binding
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        // Hiding the action bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        // Initialize FirebaseAuth instance
+        auth = FirebaseAuth.getInstance();
+
+        // Setup click listener for the login button
+        binding.loginButton.setOnClickListener(v -> {
+            String email = binding.emailEditText.getText().toString().trim();
+            String password = binding.passwordEditText.getText().toString().trim();
+
+            if (TextUtils.isEmpty(email)) {
+                binding.emailEditText.setError("Email is required");
+            }
+            if (TextUtils.isEmpty(password)) {
+                binding.passwordEditText.setError("Password is required");
+            } else {
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                FirebaseUser user = auth.getCurrentUser();
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                finish();
+                                Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
     }
 }
--- app/src/main/java/com/victor/firebaseloginsignup/SignUpActivity.java
+++ app/src/main/java/com/victor/firebaseloginsignup/SignUpActivity.java
@@ -1,14 +1,67 @@
 package com.victor.firebaseloginsignup;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.victor.firebaseloginsignup.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
+    private ActivitySignUpBinding binding;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+
+        // Initialize view binding
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Hiding the action bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        // Initialize Firebase Authentication
+        auth = FirebaseAuth.getInstance();
+
+        // Set up click listener for sign up button
+        binding.signUpButton.setOnClickListener(view -> {
+            // Get email and password from edit texts
+            String email = binding.emailEditText.getText().toString();
+            String password = binding.passwordEditText.getText().toString();
+
+            // Check if email and password are not empty
+            if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+                // Create a new user with email and password
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                // Sign up successful, go to main activity
+                                startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+                                finish();
+                            } else {
+                                // Sign up failed, display error message
+                                Toast.makeText(SignUpActivity.this, "Sign up failed. Please try again.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            } else {
+                // Email or password is empty, display error message
+                Toast.makeText(SignUpActivity.this, "Please enter email and password.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Set up click listener for already signed up text view
+        binding.alreadySignedUpTextview.setOnClickListener(view -> {
+            // Go to login activity
+            startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+            finish();
+        });
     }
 }
